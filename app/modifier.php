96bb37a62c1deb72dae199b79cc3f43b4863b50d
TonyJonathan <133671540+TonyJonathan@users.noreply.github.com>
2023-09-22 14:10:12 +0200

    Ajout de la possibilité de modifier un contact
    Récuperations des données du contact dans la base, possibilité de moification sur l'interface et envoie des nouvelles données dans la base

--- app/modifier.php
+++ app/modifier.php
@@ -1,3 +1,46 @@
+<?php
+if($_SERVER['REQUEST_METHOD'] == "POST"){
+
+    $nom = $_POST['nom']; 
+    $prenom = $_POST['prenom'];
+    $email = $_POST['email'];
+    $id = $_GET['id'];
+
+    try{
+        // Établir une connexion à la base de données avec PDO
+        $servername = "mysql:host=mysql";
+        $username = getenv("MYSQL_USER");
+        $password_db = getenv("MYSQL_PASSWORD");
+        $dbname = getenv("MYSQL_DATABASE");
+
+        $conn = new PDO("$servername;dbname=$dbname;charset=utf8", $username, $password_db);
+        
+        // Définir le mode d'erreur PDO sur exception (sert à gérer les erreurs plus facilement)
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        
+        $sql = "UPDATE contacts set nom = :nom, prenom = :prenom, email = :email WHERE id = :id";
+
+        $stmt = $conn->prepare($sql); 
+        $stmt->bindParam(':id', $id);
+        $stmt->bindParam(':nom', $nom);
+        $stmt->bindParam(':prenom', $prenom);
+        $stmt->bindParam(':email', $email);
+        $stmt->execute();
+
+        header('Location: dashbord.php');
+        exit();
+        
+    } catch (PDOException $e){
+        echo "Erreur dans l'inscription des données : " . $e->getMessage(); 
+    }
+
+    $conn = null; 
+}
+
+
+?>
+
 
 <!DOCTYPE html>
 <html lang="en">
@@ -32,67 +75,53 @@
             <!-- Formulaire d'ajout de contact -->
             <form action="" method="POST">
                 <?php
+                if (isset($_GET['id'])){
+                    $id_contact = $_GET['id']; 
+                        // Établir une connexion à la base de données avec PDO
+                        $servername = "mysql:host=mysql";
+                        $username = getenv("MYSQL_USER");
+                        $password_db = getenv("MYSQL_PASSWORD");
+                        $dbname = getenv("MYSQL_DATABASE");
 
-if (isset($_GET['id'])){
-    $id_contact = $_GET['id']; 
-        // Établir une connexion à la base de données avec PDO
-        $servername = "mysql:host=mysql";
-        $username = getenv("MYSQL_USER");
-        $password_db = getenv("MYSQL_PASSWORD");
-        $dbname = getenv("MYSQL_DATABASE");
+                        $conn = new PDO("$servername;dbname=$dbname;charset=utf8", $username, $password_db);
+                        
+                        // Définir le mode d'erreur PDO sur exception (sert à gérer les erreurs plus facilement)
+                        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-        $conn = new PDO("$servername;dbname=$dbname;charset=utf8", $username, $password_db);
-        
-        // Définir le mode d'erreur PDO sur exception (sert à gérer les erreurs plus facilement)
-        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                    try{
+                        $sql = "SELECT nom, prenom, email, utilisateur_id FROM contacts WHERE id = :id_contact";
+                        $stmt = $conn->prepare($sql); 
+                        $stmt->bindParam(':id_contact', $id_contact); 
+                        $stmt->execute(); 
+                        $results = $stmt->fetchAll(PDO::FETCH_ASSOC); 
 
-    try{
-        $sql = "SELECT nom, prenom, email, utilisateur_id FROM contacts WHERE id = :id_contact";
-        $stmt = $conn->prepare($sql); 
-        $stmt->bindParam(':id_contact', $id_contact); 
-        $stmt->execute(); 
-        $results = $stmt->fetchAll(PDO::FETCH_ASSOC); 
+                            foreach ($results as $row){
+                            $nom_contact = $row['nom'];
+                            $prenom_contact = $row['prenom']; 
+                            $email_contact = $row['email'];
+                            $utilisateur_id = $row['utilisateur_id'];
+                            
 
-            foreach ($results as $row){
-            $nom_contact = $row['nom'];
-            $prenom_contact = $row['prenom']; 
-            $email_contact = $row['email'];
-            $id_utilisateur = $row['utilisateur_id'];
-
-            echo "<div class='form-group'>
-                <label for='nom'>Nom</label>
-                <input type='text' class='form-control' id='nom' name='nom' value='$nom_contact' required>
-            </div>
-            <div class='form-group'>
-                <label for='prenom'>Prénom</label>
-                <input type='text' class='form-control' id='prenom' name='prenom' value='$prenom_contact' required>
-            </div>
-            <div class='form-group'>
-                <label for='email'>Adresse e-mail</label>
-                <input type='email' class='form-control' id='email' name='email' value='$email_contact' required>
-            </div>
-            
-            <div class='form-group'>
-                <label for='prenom'>id_utilisateur</label>
-                <input type='text' class='form-control' id='id_utilisateur' name='id_utilisateur' value='$id_utilisateur' required>
-            </div>";
-            }
+                            echo "<div class='form-group'>
+                                <label for='nom'>Nom</label>
+                                <input type='text' class='form-control' id='nom' name='nom' value='$nom_contact' required>
+                            </div>
+                            <div class='form-group'>
+                                <label for='prenom'>Prénom</label>
+                                <input type='text' class='form-control' id='prenom' name='prenom' value='$prenom_contact' required>
+                            </div>
+                            <div class='form-group'>
+                                <label for='email'>Adresse e-mail</label>
+                                <input type='email' class='form-control' id='email' name='email' value='$email_contact' required>
+                            </div>";
+                            }
 
 
-    } catch(PDOException $e){
-        echo "Erreur lors de la suppression : " . $e->getMessage(); 
-    }
+                    } catch(PDOException $e){
+                        echo "Erreur lors de la suppression : " . $e->getMessage(); 
+                    }
 
-} 
-
-
-
-
-
-
-
-                
-
+                } 
                 ?>
 
                 <button type="submit" class="btn btn-primary">Modifier</button>
